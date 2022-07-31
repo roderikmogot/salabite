54d585d6a86a553dc75a52588ed450a4d5e635e0
roderikmogot <roderikmogot17@gmail.com>
2022-07-31 16:03:33 +0800

    added last update, site meta and global table

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [showChild, setShowChild] = useState(false);
@@ -18,6 +19,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   } else {
     return (
       <>
+        <Head>
+          <title>Salabite - COVID-19 Statistics</title>
+          <meta name="description" content="COVID-19 global statistics by Mathdro API" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Navbar />
         <Component {...pageProps} />
       </>
